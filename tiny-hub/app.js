d534ea07b943cbfff29ff2611a1dbd17effa0e9d
sgregson84 <scott.gregson@gmail.com>
2020-02-29 08:15:13 -0500

    fixed formatting error.

--- tiny-hub/app.js
+++ tiny-hub/app.js
@@ -13,33 +13,6 @@ var garageDoor = {
   name:"Garage Door", 
   state: "CLOSED"
 };
-
-
-/*const server = http.createServer((req, res) => {
-
-
-
-const server = http.createServer((req, res) => {
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-
-  fs.readFile(filePath,function(error,data){
-    if(error){
-      res.writeHead(404);
-      res.write('Whoops! File not found!');
-    }else{
-      res.write(data);
-    }
-
-  console.log('New connection');
-  
-  res.end();
-  });
-
-});
-*/
-
 const deviceResponse = `
     {"status" :"COMPLETE"}
 `; 
@@ -80,69 +53,3 @@ app.post('/devices', (req, res) => {
 app.listen(port, hostname, () => {
   console.log(`Tiny Hub Server running at http://${hostname}:${port}/`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const http = require('http');
-const fs = require('fs');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-let handleRequest = (request, response) => {
-  response.writeHead(200, {
-      'Content-Type': 'text/html'
-  });
-  fs.readFile('./index.html', null, function (error, data) {
-      if (error) {
-          response.writeHead(404);
-          respone.write('Whoops! File not found!');
-      } else {
-          response.write(data);
-      }
-      response.end();
-  });
-};
-
-const server = http.createServer(handleRequest).listen(port);
-
-/*var userCount = 0;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  userCount++;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello World');
-  console.log('New connection, '+userCount+'');
-  res.write('Hello!\n');
-  res.write('We have had '+userCount+' visits!\n');
-  res.end();
-  
-});
-
-
-
-
-server.listen(port, hostname, () => {
-  console.log(`Tiny Hub Server running at http://${hostname}:${port}/`);
-});
-*/
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Tiny Hub Server running at http://${hostname}:${port}/`);
-});
-
